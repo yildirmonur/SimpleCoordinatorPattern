b099a458fe298a40e61e4132d6cbee39644fb423
Onur Yıldırım <>
2022-06-28 10:40:36 +0300

    FirstViewModel moved to a new file

--- SimpleCoordinatorPattern/SimpleCoordinatorPattern/First/FirstView.swift
+++ SimpleCoordinatorPattern/SimpleCoordinatorPattern/First/FirstView.swift
@@ -7,41 +7,6 @@
 
 import SwiftUI
 
-class FirstViewModel: ObservableObject {
-    
-    unowned let coordinator: FirstCoordinator
-    private let searchService: TeamPlayerSearchServiceProtocol
-    
-    init(coordinator: FirstCoordinator, searchService: TeamPlayerSearchServiceProtocol) {
-        self.searchService = searchService
-        self.coordinator = coordinator
-        
-        getSearchResults(with: "Ronaldo")
-    }
-    
-    func getSearchResults(with text: String, searchType: TeamPlayerSearchRequest.SearchType = .all) {
-        let request = TeamPlayerSearchRequest(searchString: text, searchType: searchType)
-        
-        Task {
-            do {
-                let result = try await searchService.search(with: request)
-                
-                // we get the result here
-                print(result)
-            }
-            catch (let error) {
-                print(error)
-            }
-        }
-    }
-    
-    @MainActor
-    func showAlert(error: CustomError) {
-        coordinator.alertItem = error.showUrlAlert()
-    }
-    
-}
-
 struct FirstView: View {
     
     @ObservedObject var viewModel: FirstViewModel
